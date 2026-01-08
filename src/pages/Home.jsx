6b6d0c3ccbbf552a9458d2ab5b96c80534ba2051
AgustinMGauto <agustin_gauto@outlook.com>
2026-01-08 18:23:51 +0000

    visualmente bien

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,11 +18,22 @@ export const Home = () => {
 			<div className="container">
 				{
 					store.contacts.map(contact => (
-						<div key={contact.id}>
+						<div key={contact.id} className="card border-dark m-3 p-3" >
+							<div className="d-flex justify-content-between">
+							<div className="d-flex justify-content-start">
+							<img src="https://imagenes.atresplayer.com/atp/clipping/cmsimages01/2020/09/15/83BD4801-852F-461A-A4F9-F61ECB2B0867//720x540.jpg" alt="" className="object-fit-cover rounded-circle" style={{width: "240px", height: "240px"}} />					
+							<div className="m-4">
 							<h3>Name: {contact.name}</h3>
-							<p>Email:{contact.email}</p>
-							<p>Phone:{contact.phone}</p>
-							<p>Address:{contact.address}</p>
+							<p>Email: <i class="fa-solid fa-envelope"></i> {contact.email}</p>
+							<p>Phone: <i class="fa-solid fa-phone"></i> {contact.phone}</p>
+							<p>Address: <i class="fa-solid fa-location-dot"></i> {contact.address}</p>
+							</div>
+							</div>
+							<div>
+								<button className="btn m-0" type="button"> <i class="fa-solid fa-pencil"></i> </button>
+								<button className="btn m-0" type="button"> <i class="fa-regular fa-trash-can"></i> </button>
+							</div>
+							</div>
 						</div>
 					))
 				}
